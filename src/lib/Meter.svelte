d973b27c1c3fabef850237f2f8ef3adb0df3771e
Frank Harkins <frankharkins+git@hotmail.co.uk>
2022-02-19 14:05:32 +0000

    style meter

--- src/lib/Meter.svelte
+++ src/lib/Meter.svelte
@@ -1,21 +1,43 @@
 <script>
+	// TODO:
+	// - Make compatible with chrome & ie
 	export let value = 30;
 </script>
 
 <style>
-	div.meter-outer {
+	div.meter-container {
 		width: 100%;
-		height: 50px;
-		border: 1px solid black;
 	}
 
-	div.meter-inner {
-		height: 100%;
-		background: blue;
-		border-right: 1px solid black;
+	input.meter {
+		-webkit-appearance: none;
+		width: 100%;
+		height: 5px;
+		background: black;
+		-webkit-transition: .2s;
+	}
+
+	input.meter::-moz-range-thumb {
+		width: 5px;
+		height: 15px;
+		background: black;
+		border: none;
+		border-radius: 0;
+	}
+
+	input.meter::-moz-range-progress {
+		background: black;
+		height: 5px;
+		border: none;
+	}
+
+	input.meter::-moz-range-track {
+		background: lightgrey;
+		height: 5px;
+		border: 1px solid white;
 	}
 </style>
 
-<div class='meter-outer'>
-	<div class='meter-inner' style='width: {value}%'></div>
+<div class='meter-container'>
+	<input type="range" min="0" max="1000" value="{value*10}" class="meter" disabled="true">
 </div>
