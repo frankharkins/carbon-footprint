a27ba5a7de0d3362b9d4c1c5d26fa898b6d40d46
Frank Harkins <frankharkins+git@hotmail.co.uk>
2022-02-23 21:37:52 +0000

    Replace svelte-select with custom component

--- src/lib/Dropdown.svelte
+++ src/lib/Dropdown.svelte
@@ -1,11 +1,53 @@
 <script>
-	import Select from 'svelte-select';
-	export let items = [0];
-	export let value = options[0];
+	export let items = {label: "No values specified", value: 1};
+	export let value = items[0];
 
-	function handleSelect(event) {
-		value = event.detail;
+	function handleSelect(item) {
+		value = item;
+		console.log(value);
 	}
 </script>
 
-<Select {items} {value} on:select={handleSelect}></Select>
+<style>
+	.dropdown {
+		display: inline-block;
+		cursor: pointer;
+		border-bottom: 1px solid black;
+	}
+
+	.menu {
+		visibility: hidden;
+		position: absolute;
+		padding: 2px;
+		background: white;
+		cursor: pointer;
+	}
+
+	.item {
+		border-bottom: 1px solid white;
+	}
+
+	.item:hover {
+		border-bottom: 1px solid black;
+	}
+
+	.dropdown:hover {
+		border-bottom: 1px solid white;
+	}
+
+	.dropdown:hover > .menu {
+		visibility: visible;
+	}
+
+</style>
+
+<span class="dropdown">
+	{value.label}
+	<div class="menu">
+		{#each items as item}
+			{#if item != value}
+				<div class="item" on:click={handleSelect(item)}>{item.label}</div>
+			{/if}
+		{/each}
+	</div>
+</span>
