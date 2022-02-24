4f2f46d5c5979786ee957dcade4ab4bf8d4cb650
Frank Harkins <frankharkins+git@hotmail.co.uk>
2022-02-24 22:15:54 +0000

    make comparison panel

--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,12 @@
 <script>
   import Travel from './lib/Travel.svelte'
-  import Meter from './lib/Meter.svelte'
+  import ComparisonPanel from './lib/ComparisonPanel.svelte'
 
   let travel_carbon = 0;
   $: meter_value = travel_carbon/30;
 </script>
 
 <main>
-<Meter bind:value={meter_value}/>
+<ComparisonPanel bind:value={meter_value}/>
 <Travel bind:total_carbon={travel_carbon}/>
 </main>
---
+++ src/lib/ComparisonPanel.svelte
@@ -0,0 +1,12 @@
+<script>
+	export let value = 30;
+	import Panel from './Panel.svelte'
+	import Meter from './Meter.svelte'
+</script>
+
+<Panel width="1200">
+	Your footprint:
+	<Meter bind:value={value}/>
+	UK average:
+	<Meter value="30"/>
+</Panel>
