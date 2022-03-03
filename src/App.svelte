28321a154ac079afcb6a9099ae993dc19b9426fa
Frank Harkins <frankharkins+git@hotmail.co.uk>
2022-03-03 16:55:36 +0000

    Add prototype diet panel

--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,15 @@
 <script>
   import Travel from './lib/Travel.svelte'
+  import Diet from './lib/Diet.svelte'
   import ComparisonPanel from './lib/ComparisonPanel.svelte'
 
   let travel_carbon = 0;
-  $: meter_value = travel_carbon/30;
+  let diet_carbon = 0
+  $: meter_value = (travel_carbon + diet_carbon)/30;
 </script>
 
 <main>
 <ComparisonPanel bind:value={meter_value}/>
 <Travel bind:total_carbon={travel_carbon}/>
+<Diet bind:total_carbon={diet_carbon}/>
 </main>
---
+++ src/lib/Diet.svelte
@@ -0,0 +1,58 @@
+<script>
+	import Dropdown from './Dropdown.svelte';
+	import Panel from './Panel.svelte';
+	export let total_carbon;
+	
+	// define choices user can select from
+	let mass_units = [
+		{ value: 'grams',  label: 'grams'  },
+		{ value: 'ounces', label: 'ounces' }
+	];
+	let time_units = [
+		{ label: 'day',   value: 365 },
+		{ label: 'week',  value: 52  },
+		{ label: 'month', value: 12  },
+		{ label: 'year',  value: 1   }
+	];
+	let food_types = [ // 'value' == GHG per 100g
+		{ label: 'beef',   value: 50/100 },
+		{ label: 'cheese', value: 11/100 },
+		{ label: 'tofu',   value:  2/100 }
+	];
+	for (let i=food_types.length; i--;) {
+		food_types[i].mass = 0;
+		food_types[i].units = mass_units[0];
+		food_types[i].period = time_units[1];
+	};
+
+	// calculate carbon emissions (kg) and export
+	function get_carbon(food_type) {
+		let mass = food_type.mass;
+		if (food_type.units.value === 'ounces') {
+			mass = mass / 28.35;
+		};
+		let per_year = mass * food_type.value;
+		return per_year * food_type.period.value;
+	};
+	
+	function sum_carbon(food_types) {
+		let total = 0;
+		for (var i=food_types.length; i--;) {
+			total += get_carbon(food_types[i])
+		};
+		return total;
+	};
+
+	$: total_carbon = sum_carbon(food_types);
+</script>
+<Panel>
+	{#each food_types as food}
+		<p>
+		<Dropdown bind:value={food.units} items={mass_units}/>
+		of {food.label} bought per
+		<Dropdown bind:value={food.period} items={time_units}/>
+		:</p>
+		<input type='number' min='0' bind:value={food.mass}>
+	{/each}
+	<p>Your diet emits ~{total_carbon.toFixed(2)}kg of co2 per year</p>
+</Panel>
