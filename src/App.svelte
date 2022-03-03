3abbd8100359669b449ef892d9dc42b595c28c4b
Frank Harkins <frankharkins+git@hotmail.co.uk>
2022-03-03 17:09:39 +0000

    Add raw emissions component

--- src/App.svelte
+++ src/App.svelte
@@ -1,15 +1,18 @@
 <script>
   import Travel from './lib/Travel.svelte'
   import Diet from './lib/Diet.svelte'
+  import Raw from './lib/Raw.svelte'
   import ComparisonPanel from './lib/ComparisonPanel.svelte'
 
   let travel_carbon = 0;
-  let diet_carbon = 0
-  $: meter_value = (travel_carbon + diet_carbon)/30;
+  let diet_carbon = 0;
+  let raw_carbon = 0;
+  $: meter_value = (travel_carbon + diet_carbon + raw_carbon)/30;
 </script>
 
 <main>
 <ComparisonPanel bind:value={meter_value}/>
 <Travel bind:total_carbon={travel_carbon}/>
 <Diet bind:total_carbon={diet_carbon}/>
+<Raw bind:total_carbon={raw_carbon}/>
 </main>
---
+++ src/lib/Raw.svelte
@@ -0,0 +1,23 @@
+<script>
+	import Dropdown from './Dropdown.svelte';
+	import Panel from './Panel.svelte';
+	export let total_carbon;
+	
+	// user can choose time period
+	let time_units = [
+		{ label: 'day',   value: 365 },
+		{ label: 'week',  value: 52  },
+		{ label: 'month', value: 12  },
+		{ label: 'year',  value: 1   }
+	];
+	
+	let selected_units = time_units[3];
+	let carbon = 0;
+	$: total_carbon = carbon * selected_units.value;
+</script>
+<Panel>
+	<p>Enter additional emissions (in kg) per
+	<Dropdown bind:value={selected_units} items={time_units}/>
+	:</p>
+	<input type='number' min='0' bind:value={carbon}>
+</Panel>
