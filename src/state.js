7424433b78feb7291e9d93b8662bc59c4d323449
Frank Harkins <frankharkins+git@hotmail.co.uk>
2022-03-16 22:17:53 +0000

    use eslint

--- src/state.js
+++ src/state.js
@@ -1,15 +1,15 @@
-import { writable } from 'svelte/store';
+import { writable } from "svelte/store";
 
-let APP_STATES = ['travel', 'diet', 'raw', 'play'];
+let APP_STATES = ["travel", "diet", "raw", "play"];
 
-export const appState = writable(APP_STATES[0])
+export const appState = writable(APP_STATES[0]);
 
 appState.subscribe( (state) => {
-	if (state === 'play') {
-		window.showPopup('comparison');
+	if (state === "play") {
+		window.showPopup("comparison");
 	}
-})
+});
 
 export function nextAppState(_) {
-	appState.update(state => APP_STATES[ APP_STATES.indexOf(state) + 1])
+	appState.update(state => APP_STATES[ APP_STATES.indexOf(state) + 1]);
 }
